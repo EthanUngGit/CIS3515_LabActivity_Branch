5d82959857eadbe888024d942bb24129fd4f2621
EthanUngGit <ethan.ung@temple.edu>
2024-02-21 15:35:59 -0500

    Set error for empty user input

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -17,12 +17,16 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
-        
-        
+        var editTextView = findViewById<EditText>(R.id.nameEditText)
+
+
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            if (editTextView.text.isEmpty()) {
+                editTextView.setError("Name not specified")
+            }
+            else {
+                displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            }
         }
-
-
     }
 }
